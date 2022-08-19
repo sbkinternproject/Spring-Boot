086e3584c63e19d14293517e811dc810c19a24d1
Chirag Sardana <macbookair@MacBook-Air.local>
2022-08-19 20:28:08 +0530

    Adding Main Controller

---
+++ src/main/java/com/sbk/MicroserviceBasedApplication/controller/MainController.java
@@ -0,0 +1,16 @@
+package com.sbk.MicroserviceBasedApplication.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/")
+public class MainController {
+    @GetMapping()
+    @ResponseBody
+    public String homePage(){
+        return "<h1><center>Welcome to Microservice Based Application By Chirag19CSU071</center></h1>";
+    }
+}
