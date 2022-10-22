b11c52d78fc75f87fb1288e62525aaca5307b304
Chirag Sardana <macbookair@MacBook-Air.local>
2022-10-22 22:44:20 +0530

    Dish Controller

---
+++ src/main/java/com/sbk/MicroserviceBasedApplication/controller/DishController.java
@@ -0,0 +1,53 @@
+package com.sbk.MicroserviceBasedApplication.controller;
+
+
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.sbk.MicroserviceBasedApplication.model.Dish;
+import com.sbk.MicroserviceBasedApplication.service.DishService;
+
+@CrossOrigin(origins = "*", allowedHeaders = "*")
+@RestController
+@RequestMapping("/Dish")
+public class DishController {
+    private DishService dishService;
+
+    public DishController(DishService dishService) {
+        this.dishService = dishService;
+    }
+    @PostMapping("/save")
+    public ResponseEntity<Dish> saveExpense(@RequestBody Dish dish){
+        return new ResponseEntity<Dish>(dishService.save(dish), HttpStatus.CREATED);
+    }
+    @GetMapping("/get")
+    public ResponseEntity<List<Dish>> get(){
+        return new ResponseEntity<List<Dish>>(dishService.getAllDishes(), HttpStatus.OK);
+    }
+    @GetMapping("/getById/{id}")
+    public ResponseEntity<Dish> getExpenseById(@PathVariable("id") Long id){
+        // System.out.println("The Id we are getting is "+id);
+        return new ResponseEntity<Dish>(dishService.getDishById(id), HttpStatus.OK);
+    }
+    @DeleteMapping("/deleteById/{id}")
+    public ResponseEntity<Boolean> deleteExpenseById(@PathVariable("id") Long id){
+        // System.out.println("The Id we are getting is "+id);
+        return new ResponseEntity<Boolean>(dishService.deleteDishById(id), HttpStatus.OK);
+    }
+    @PutMapping("/updateById/{id}")
+    public ResponseEntity<Dish> updateExpenseById(@PathVariable("id") Long id, @RequestBody Dish Dish){
+        return new ResponseEntity<Dish>(dishService.updateDishById(id, Dish), HttpStatus.OK);
+    }
+
+}
