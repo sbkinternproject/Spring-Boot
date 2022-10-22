1b73fc4d1f9402c96c9e4d36d4b6fd719e0b3a12
Chirag Sardana <macbookair@MacBook-Air.local>
2022-10-22 22:55:24 +0530

    Rating Controller

---
+++ src/main/java/com/sbk/MicroserviceBasedApplication/controller/RatingController.java
@@ -0,0 +1,54 @@
+
+package com.sbk.MicroserviceBasedApplication.controller;
+
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.sbk.MicroserviceBasedApplication.model.Rating;
+import com.sbk.MicroserviceBasedApplication.service.RatingService;
+
+@CrossOrigin(origins = "*", allowedHeaders = "*")
+@RestController
+@RequestMapping("/Rating")
+public class RatingController {
+    private RatingService ratingService;
+
+    public RatingController(RatingService ratingService) {
+        this.ratingService = ratingService;
+    }
+    @PostMapping("/save")
+    public ResponseEntity<Rating> saveExpense(@RequestBody Rating rating){
+        return new ResponseEntity<Rating>(ratingService.save(rating), HttpStatus.CREATED);
+    }
+    @GetMapping("/get")
+    public ResponseEntity<List<Rating>> get(){
+        return new ResponseEntity<List<Rating>>(ratingService.getAllRating(), HttpStatus.OK);
+    }
+    @GetMapping("/getById/{id}")
+    public ResponseEntity<Rating> getExpenseById(@PathVariable("id") Long id){
+        // System.out.println("The Id we are getting is "+id);
+        return new ResponseEntity<Rating>(ratingService.getRatingById(id), HttpStatus.OK);
+    }
+    @DeleteMapping("/deleteById/{id}")
+    public ResponseEntity<Boolean> deleteExpenseById(@PathVariable("id") Long id){
+        // System.out.println("The Id we are getting is "+id);
+        return new ResponseEntity<Boolean>(ratingService.deleteRatingById(id), HttpStatus.OK);
+    }
+    @PutMapping("/updateById/{id}")
+    public ResponseEntity<Rating> updateExpenseById(@PathVariable("id") Long id, @RequestBody Rating Rating){
+        return new ResponseEntity<Rating>(ratingService.updateRatingById(id, Rating), HttpStatus.OK);
+    }
+
+}
+
